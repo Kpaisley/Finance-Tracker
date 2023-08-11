b318582d5a90aadd1a2b464be83421491723b44a
Kyle <Kpaisley73@gmail.com>
2023-08-10 19:15:51 -0700

    Added Login and Logout functionality using Auth0.

--- FinanceTracker/ClientApp/src/App.js
+++ FinanceTracker/ClientApp/src/App.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import AppRoutes from './AppRoutes';
+import { Counter } from './components/Counter';
+import { FetchData } from './components/FetchData';
+import { Home } from './components/Home';
 import { Layout } from './components/Layout';
 import './custom.css';
 
 export default class App extends Component {
-  static displayName = App.name;
+
 
   render() {
     return (
       <Layout>
-        <Routes>
-          {AppRoutes.map((route, index) => {
-            const { element, ...rest } = route;
-            return <Route key={index} {...rest} element={element} />;
-          })}
+            <Routes>
+                <Route element={<Home />} index='true'></Route>
+                <Route element={<Counter />} path='/counter'></Route>
+                <Route element={<FetchData />} path='/fetch-data'></Route>
         </Routes>
       </Layout>
     );
--- FinanceTracker/ClientApp/src/index.js
+++ FinanceTracker/ClientApp/src/index.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
@@ -11,9 +12,19 @@ const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
-  <BrowserRouter basename={baseUrl}>
-    <App />
-  </BrowserRouter>);
+
+    <Auth0Provider
+        domain="kyle-paisley-demos.us.auth0.com"
+        clientId="rAaSkFZZNpLyKyWkhH9ss67e6vruSyyu"
+        authorizationParams={{
+            redirect_uri: window.location.origin
+        }}
+    >
+        <BrowserRouter basename={baseUrl}>
+            <App />
+        </BrowserRouter>
+    </Auth0Provider>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
