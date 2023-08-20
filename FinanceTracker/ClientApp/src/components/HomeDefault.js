091edc47cdae4c08cfa6a972a65464bf735cc7ed
Kyle <Kpaisley73@gmail.com>
2023-08-20 14:52:08 -0700

    Changed HomeDefault.js background to cover 100vw of the users screen size.

--- FinanceTracker/ClientApp/src/components/HomeDefault.js
+++ FinanceTracker/ClientApp/src/components/HomeDefault.js
@@ -7,13 +7,15 @@ export const HomeDefault = (props) => {
     const { loginWithRedirect } = useAuth0();
 
     return (
-        <div id="default-container">
-            
+        <div id="home-default">
+            <div className="finance-bg">
                 <h1>BUDGET FLOW</h1>
                 <h3>TRACK YOUR SPENDING TODAY</h3>
                 <button onClick={() => loginWithRedirect()}>GET STARTED!</button>
-            
+            </div>
         </div>
+
+        
     );
 
 
