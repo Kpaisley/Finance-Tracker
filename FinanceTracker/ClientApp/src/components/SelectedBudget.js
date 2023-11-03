238e7c2177ff0728898b4343db72014f2dc49e13
Kyle <Kpaisley73@gmail.com>
2023-11-02 18:07:29 -0600

    Redesigned SelectedBudget,js to prevent endless loop when calling CategoriesController API.

--- FinanceTracker/ClientApp/src/components/SelectedBudget.js
+++ FinanceTracker/ClientApp/src/components/SelectedBudget.js
@@ -1,68 +1,133 @@
-﻿import { useState } from 'react';
+﻿import { useEffect, useState } from 'react';
 import { useLocation, Link } from 'react-router-dom';
 import { AddCategory } from './AddCategory';
 import { LogoutButton } from "./LogoutButton";
 import './SelectedBudget.css';
 
 
-export const SelectedBudget = (props) => {
+export const SelectedBudget = () => {
     const location = useLocation();
     const [categories, setCategories] = useState([]);
     const [categoriesLoading, setCategoriesLoading] = useState(true);
-    
-    //Only render if a user navigated to here by clicking a budget on the dashboard.
-    if (location.state) {
-        const budget = location.state.budget.budget;
-
-        populateCategories();
 
 
-        async function populateCategories() {
-            
-            const userId = budget.userId;
-            const budgetId = budget.id;
-            try {
-                
-                const response = await fetch('categories/' + userId + "/" + budgetId);
-                const data = await response.json();
-                setCategories(data);
-                setCategoriesLoading(false);
-                console.log(data);
-            }
-            catch (error) {
-                console.log(error);
-            }
+    //Populate Categories from CategoriesController.
+    async function populateCategories() {
 
-            
+        const userId = budget.userId;
+        const budgetId = budget.id;
+        try {
+
+            const response = await fetch('categories/' + userId + "/" + budgetId);
+            const data = await response.json();
+            setCategories(data);
+            setCategoriesLoading(false);
+            console.log(data);
+        }
+        catch (error) {
+            console.log(error);
         }
 
-        
-        
 
-        return budget && (
-            <div className="selected-budget noselect">
-                <div className="budget-title">
-                    <LogoutButton />
-                    <h2>{budget.budgetName}</h2>
-                    <Link className='return-link' to='/'>Return</Link>
-                    <div className='date-modified'>
-                        <u>Last Modified</u>
-                        <p>{budget.dateLastModified.slice(0, 10)}</p>
-                    </div>
-                    
+    }
 
-                   <AddCategory />
+    useEffect(() => {
+        populateCategories();
+    }, [])
 
-                </div>
-                {categories.length}
-                
-
-            </div>
+    //Return user to the dashboard if URL is manually typed in.
+    if (!location.state) {
+        return (
+            window.location.href = ""
         );
     }
 
-    //If user manually types in hyperlink, redirect them to the dashboard to prevent errors.
-    else
-        window.location.href = "";
-   
+
+    const budget = location.state.budget.budget;
+
+    return budget && (
+        
+        <div className="selected-budget noselect">
+            <div className="budget-title">
+                <LogoutButton />
+                <h2>{budget.budgetName}</h2>
+                <Link className='return-link' to='/'>Return</Link>
+                <div className='date-modified'>
+                    <u>Last Modified</u>
+                    <p>{budget.dateLastModified.slice(0, 10)}</p>
+                </div>
+
+
+                <AddCategory />
+
+            </div>
+            {categories.length}
+
+
+        </div>
+    );
+
 }
+
+
+//export const SelectedBudget = () => {
+//    const location = useLocation();
+//    const [categories, setCategories] = useState([]);
+//    const [categoriesLoading, setCategoriesLoading] = useState(true);
+    
+//    //Only render if a user navigated to here by clicking a budget on the dashboard.
+//    if (location.state) {
+//        const budget = location.state.budget.budget;
+
+//        populateCategories();
+
+
+//        async function populateCategories() {
+            
+//            const userId = budget.userId;
+//            const budgetId = budget.id;
+//            try {
+                
+//                const response = await fetch('categories/' + userId + "/" + budgetId);
+//                const data = await response.json();
+//                //setCategories(data);
+//                //setCategoriesLoading(false);
+//                console.log(data);
+//            }
+//            catch (error) {
+//                console.log(error);
+//            }
+
+            
+//        }
+
+        
+        
+
+//        return budget && (
+//            <div className="selected-budget noselect">
+//                <div className="budget-title">
+//                    <LogoutButton />
+//                    <h2>{budget.budgetName}</h2>
+//                    <Link className='return-link' to='/'>Return</Link>
+//                    <div className='date-modified'>
+//                        <u>Last Modified</u>
+//                        <p>{budget.dateLastModified.slice(0, 10)}</p>
+//                    </div>
+                    
+
+//                   <AddCategory />
+
+//                </div>
+//                {categories.length}
+                
+
+//            </div>
+//        );
+//    }
+
+//    //If user manually types in hyperlink, redirect them to the dashboard to prevent errors.
+//    else
+//        window.location.href = "";
+   
+//}
